61a9d29880a26707b476ef87f0968f48bc34789e
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-28 23:47:15 +0300

    Add files via upload

---
+++ docs/structtx_dialog.js
@@ -0,0 +1,21 @@
+var structtx_dialog =
+[
+    [ "this_t", "structtx_dialog.html#a1b6915fbb70258955c5b3de1a09c99e4", null ],
+    [ "CONTROL", "group___dialogs.html#ga5b47575087d696b5fddf972ce4d83657", [
+      [ "DIALOG", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657a1988e748c6846b398b020c0cb6f057ab", null ],
+      [ "BUTTON", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657aa4f15111fd03f7781dd5e910c9476085", null ],
+      [ "EDIT", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657aef6361d5dcbf8ce520f398a079b414cb", null ],
+      [ "STATIC", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657aec6c84113cb4f1cd430238d4b909b40a", null ],
+      [ "LISTBOX", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657a71c9095b5417af67a6a9b4a5140a09ea", null ],
+      [ "SCROLLBAR", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657af7644a14f317118ad12a6c5eaa216e9f", null ],
+      [ "COMBOBOX", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657a5e7719d9f62ad1839c67e21e8ebf64e2", null ],
+      [ "END", "group___dialogs.html#gga5b47575087d696b5fddf972ce4d83657a342a4f0af79f46e07894a95e5951b7aa", null ]
+    ] ],
+    [ "txDialog", "structtx_dialog.html#a081a2029ed8e4ffd6726c9f39966a04c", null ],
+    [ "txDialog", "structtx_dialog.html#a4ac4c473e24014b82cb62133f8c5ed4a", null ],
+    [ "~txDialog", "structtx_dialog.html#a0a41d301a3d12cd2542f609bb77d81a1", null ],
+    [ "dialogBox", "structtx_dialog.html#a7b075b1d868782be0973d6eb82121b94", null ],
+    [ "dialogBox", "structtx_dialog.html#a0601dff1e04c3f06c23fc443eb10093c", null ],
+    [ "dialogProc", "structtx_dialog.html#a39efe9383833aedb0839d2519173930e", null ],
+    [ "setLayout", "structtx_dialog.html#acc3c6561d4b13780848b7b5ff032f2e4", null ]
+];
