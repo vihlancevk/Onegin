b7b37c515d92382be3a1972e0e9c2711f23345ed
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-09-01 08:18:27 +0300

    Add files via upload

---
+++ docs/html/file_operations_8cpp.js
@@ -0,0 +1,8 @@
+var file_operations_8cpp =
+[
+    [ "countNumberLines", "file_operations_8cpp.html#ad6f4656b11adda4d4343d762f5d4793d", null ],
+    [ "getFileSize", "file_operations_8cpp.html#a0d2b1b51559c0a73244ddc8e999ebae3", null ],
+    [ "readFile", "file_operations_8cpp.html#a94b6e012610cd9bd2e8ba11c6c1bd28b", null ],
+    [ "splitToLines", "file_operations_8cpp.html#a8fcb01f8d35acfd54ac4b4febbd3bd5d", null ],
+    [ "writeFile", "file_operations_8cpp.html#a6cf53ddd77415cef61ffafdd91c2219c", null ]
+];
---
+++ docs/html/sort_text_8cpp.js
@@ -0,0 +1,10 @@
+var sort_text_8cpp =
+[
+    [ "compareLine", "sort_text_8cpp.html#ab0899491d14c0eee190472daf647b97d", null ],
+    [ "compareLineReverse", "sort_text_8cpp.html#a7df2e0795bef34ac05c0515bf75e32c0", null ],
+    [ "isPunctuationMark", "sort_text_8cpp.html#a0ad55b1d5bcb6a7e2c3b3ba9a1c289f0", null ],
+    [ "qsort", "sort_text_8cpp.html#a9a8e6b1637abfbd3a3f3b5da7c7962f3", null ],
+    [ "skipPunctuationMark", "sort_text_8cpp.html#a66f13211ea08df4127895226e1dce641", null ],
+    [ "swap", "sort_text_8cpp.html#a1ff0359934cade5d845714d77ca74012", null ],
+    [ "PUNCTUATION_MARKS", "sort_text_8cpp.html#a54a6d93f65525838942b8b29a25e6ce0", null ]
+];
